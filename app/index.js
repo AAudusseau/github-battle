78714033772126425486eedecd40de86e99fbf21
Antoine Antana Anahid <Antoine & Anahid>
2016-03-14 19:51:17 +0100

    Nested components

--- app/index.js
+++ app/index.js
@@ -1,15 +1,53 @@
+var USER_DATA = {
+	name: 'Antoine Audusseau',
+	username: 'aaudusseau',
+	image: 'https://avatars0.githubusercontent.com/u/4435464'
+};
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
-  render: function() {
-    return (
-      <div> Hello World React Program!</div>
-    )
-  }
+var ProfilePic = React.createClass({
+	render: function() {
+		return (
+			<img src={this.props.imageUrl} style={{height:100, width:100}} />
+			)
+	}
+});
+
+var ProfileLink = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<a href={'https://www.github.com/' + this.props.username}>
+					{this.props.username}
+				</a>
+			</div>
+			)
+	}
+});
+
+var ProfileName = React.createClass({
+	render: function() {
+		return (
+			<div>{this.props.name}</div>
+			)
+	}
+});
+
+var Avatar = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<ProfilePic imageUrl={this.props.user.image} />
+				<ProfileName name={this.props.user.name} />
+				<ProfileLink username={this.props.user.username} />
+			</div>
+			)
+	}
 });
 
 ReactDOM.render(
-  <HelloWorld />,
-  document.getElementById('app')
-);
+	<Avatar user={USER_DATA} />,
+	document.getElementById('app')
+	);
